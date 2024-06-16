c3f0111e45c02be90ec48f6065baaba0b0ab1aff
badeai <badeaionut@icloud.com>
2024-06-16 21:35:27 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   String buttonName = 'Click';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
